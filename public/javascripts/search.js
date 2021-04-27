39f31802dfe56ec0afcd17e81365a8dd9ee312c8
Ted Anton <tedjanton@gmail.com>
2021-04-27 10:54:27 -0500

    removed search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,30 +1,24 @@
-
 window.addEventListener('DOMContentLoaded', event =>{
-
 
   const searchBar = document.getElementById('searchBar')
   searchBar.addEventListener('keyup',async (event) =>{
     console.log(searchBar.value);
-     let input = event.target.value 
-  
-    
-    input=input.toLowerCase(); 
-    let x = document.querySelectorAll('.game-container'); 
-    
-    for (i = 0; i < x.length; i++) {  
-        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-            x[i].style.display="none"; 
-            
-        } 
-        else { 
-            x[i].style.display="list";                  
-        } 
-    } 
-  
+     let input = event.target.value
+
+
+    input=input.toLowerCase();
+    let x = document.querySelectorAll('.game-container');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+
+        }
+        else {
+            x[i].style.display="list";
+        }
+    }
+
   })
-  
-  
-  
-  })
-  
-  
+
+})
