0993e7f3e05370695dfc653a9ce1543885f16891
Ted Anton <tedjanton@gmail.com>
2021-04-27 17:16:44 -0500

    added thumbnail meta tag

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,24 +1,24 @@
-window.addEventListener('DOMContentLoaded', event =>{
+// window.addEventListener('DOMContentLoaded', event =>{
 
-  const searchBar = document.getElementById('searchBar')
-  searchBar.addEventListener('keyup',async (event) =>{
-    console.log(searchBar.value);
-     let input = event.target.value
+//   const searchBar = document.getElementById('searchBar')
+//   searchBar.addEventListener('keyup',async (event) =>{
+//     console.log(searchBar.value);
+//      let input = event.target.value
 
 
-    input=input.toLowerCase();
-    let x = document.querySelectorAll('.game-container');
+//     input=input.toLowerCase();
+//     let x = document.querySelectorAll('.game-container');
 
-    for (i = 0; i < x.length; i++) {
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
+//     for (i = 0; i < x.length; i++) {
+//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
+//             x[i].style.display="none";
 
-        }
-        else {
-            x[i].style.display="list";
-        }
-    }
+//         }
+//         else {
+//             x[i].style.display="list";
+//         }
+//     }
 
-  })
+//   })
 
-})
+// })
